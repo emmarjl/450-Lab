e20b8116a94932ef7a44080921ca1bfb2fd715e9
Emma Lopez <143381627+emmarjl@users.noreply.github.com>
2026-09-25 13:07:38 -0700

    Implement FSM for traffic light control

--- traffic_light_phase1_v2.ino
+++ traffic_light_phase1_v2.ino
@@ -1,242 +1,83 @@
 #include <Arduino.h>
 
-#define NORTH_SENSOR_PIN 21   // Left button
-#define EAST_SENSOR_PIN  20   // Right button
-
-// North/South traffic light
-#define NS_GREEN_PIN   13
-#define NS_YELLOW_PIN  15
-#define NS_RED_PIN      4
-
-// East/West traffic light
-#define EW_RED_PIN     27
-#define EW_YELLOW_PIN  26
-#define EW_GREEN_PIN   25
-
-// FSM States
-
-enum State {
-  N_GREEN,
-  N_YELLOW,
-  ALL_RED_1,
-  E_GREEN,
-  E_YELLOW,
-  ALL_RED_2
+struct State {
+  unsigned long int ledOutput;
+  unsigned long time;
+  byte nextState[4];
 };
 
-State state = N_GREEN;
+typedef const struct State STyp;
 
-// Turn all LEDs off
-void allLightsOff()
-{
-  digitalWrite(NS_GREEN_PIN, LOW);
-  digitalWrite(NS_YELLOW_PIN, LOW);
-  digitalWrite(NS_RED_PIN, LOW);
+// Pin definitions
+const int northGreen = 13;
+const int northYellow = 15;
+const int northRed = 4;
+const int eastRed = 27;
+const int eastYellow = 26;
+const int eastGreen = 25;
+const int northButton = 21;
+const int eastButton = 20;
 
-  digitalWrite(EW_RED_PIN, LOW);
-  digitalWrite(EW_YELLOW_PIN, LOW);
-  digitalWrite(EW_GREEN_PIN, LOW);
+// State indices
+#define NORTH_GREEN 0
+#define NORTH_YELLOW 1
+#define EAST_GREEN 2
+#define EAST_YELLOW 3
+
+// Input: 00 = none, 01 = East, 10 = North, 11 = both
+// LEDs: bit 0 = NG, bit 1 = NY, bit 2 = NR
+//       bit 3 = EG, bit 4 = EY, bit 5 = ER
+
+STyp FSM[4] = {
+  // North Green
+  {0b100001,3000,
+   {NORTH_GREEN,NORTH_YELLOW,NORTH_GREEN,NORTH_YELLOW}},
+
+  // North Yellow
+  {0b100010,1000,
+   {EAST_GREEN,EAST_GREEN,EAST_GREEN,EAST_GREEN}},
+
+  // East Green
+  {0b001100,3000,
+   {EAST_GREEN,EAST_GREEN,EAST_YELLOW,EAST_YELLOW}},
+
+  // East Yellow
+  {0b010100,1000,
+   {NORTH_GREEN,NORTH_GREEN,NORTH_GREEN,NORTH_GREEN}}
+};
+
+byte currentState = NORTH_GREEN;
+
+void setLights(unsigned long pattern) {
+  digitalWrite(northGreen,(pattern & 0x01) ? HIGH : LOW);
+  digitalWrite(northYellow,(pattern & 0x02) ? HIGH : LOW);
+  digitalWrite(northRed,(pattern & 0x04) ? HIGH : LOW);
+  digitalWrite(eastGreen,(pattern & 0x08) ? HIGH : LOW);
+  digitalWrite(eastYellow,(pattern & 0x10) ? HIGH : LOW);
+  digitalWrite(eastRed,(pattern & 0x20) ? HIGH : LOW);
 }
 
+void setup() {
+  pinMode(northGreen,OUTPUT);
+  pinMode(northYellow,OUTPUT);
+  pinMode(northRed,OUTPUT);
+  pinMode(eastRed,OUTPUT);
+  pinMode(eastYellow,OUTPUT);
+  pinMode(eastGreen,OUTPUT);
+  pinMode(northButton,INPUT);
+  pinMode(eastButton,INPUT);
 
-// Set LEDs according to current state
-void setLights(State currentState)
-{
-  // Turn everything off first
-  allLightsOff();
-
-  switch (currentState)
-  {
-    case N_GREEN:
-      digitalWrite(NS_GREEN_PIN, HIGH);
-      digitalWrite(EW_RED_PIN, HIGH);
-      break;
-
-    case N_YELLOW:
-      digitalWrite(NS_YELLOW_PIN, HIGH);
-      digitalWrite(EW_RED_PIN, HIGH);
-      break;
-
-    case ALL_RED_1:
-      digitalWrite(NS_RED_PIN, HIGH);
-      digitalWrite(EW_RED_PIN, HIGH);
-      break;
-
-    case E_GREEN:
-      digitalWrite(NS_RED_PIN, HIGH);
-      digitalWrite(EW_GREEN_PIN, HIGH);
-      break;
-
-    case E_YELLOW:
-      digitalWrite(NS_RED_PIN, HIGH);
-      digitalWrite(EW_YELLOW_PIN, HIGH);
-      break;
-
-    case ALL_RED_2:
-      digitalWrite(NS_RED_PIN, HIGH);
-      digitalWrite(EW_RED_PIN, HIGH);
-      break;
-  }
+  setLights(FSM[currentState].ledOutput);
 }
 
+void loop() {
+  byte northPressed = digitalRead(northButton);
+  byte eastPressed = digitalRead(eastButton);
+  byte inputIndex = (northPressed << 1) | eastPressed;
 
-// Read the two buttons
-// bit 1 = North
-// bit 0 = East
-uint8_t readInputs()
-{
-  uint8_t north = digitalRead(NORTH_SENSOR_PIN);
-  uint8_t east  = digitalRead(EAST_SENSOR_PIN);
+  delay(FSM[currentState].time);
 
-  return (north << 1) | east;
-}
+  currentState = FSM[currentState].nextState[inputIndex];
 
-
-
-void setup()
-{
-  // Button inputs
-  // ESP32's internal pull-down resistors
-  pinMode(NORTH_SENSOR_PIN, INPUT_PULLDOWN);
-  pinMode(EAST_SENSOR_PIN, INPUT_PULLDOWN);
-
-  // LED outputs
-  pinMode(NS_GREEN_PIN, OUTPUT);
-  pinMode(NS_YELLOW_PIN, OUTPUT);
-  pinMode(NS_RED_PIN, OUTPUT);
-
-  pinMode(EW_RED_PIN, OUTPUT);
-  pinMode(EW_YELLOW_PIN, OUTPUT);
-  pinMode(EW_GREEN_PIN, OUTPUT);
-
-  // Start with everything off
-  allLightsOff();
-}
-
-
-
-void loop()
-{
-
-  // NORTH GREEN
-  if (state == N_GREEN)
-  {
-    setLights(N_GREEN);
-
-    delay(3000);
-
-    // Read buttons
-    uint8_t input = readInputs();
-
-    // 00 = no cars
-    // 01 = East has car
-    // 10 = North has car
-    // 11 = both have cars
-
-    if (input == 0b00)
-    {
-      state = N_GREEN;
-    }
-    else
-    {
-      state = N_YELLOW;
-    }
-  }
-
-  // NORTH YELLOW
-
-  else if (state == N_YELLOW)
-  {
-    setLights(N_YELLOW);
-
-    delay(1000);
-
-    state = ALL_RED_1;
-  }
-
-
-  // ALL RED 1
-  else if (state == ALL_RED_1)
-  {
-    setLights(ALL_RED_1);
-
-    delay(500);
-
-    uint8_t input = readInputs();
-
-    if (input == 0b01)
-    {
-      // East car only
-      state = E_GREEN;
-    }
-    else if (input == 0b11)
-    {
-      // Both have cars
-      state = E_GREEN;
-    }
-    else
-    {
-      // North only or nobody
-      state = N_GREEN;
-    }
-  }
-
-
-  // EAST GREEN
-  else if (state == E_GREEN)
-  {
-    setLights(E_GREEN);
-
-    delay(3000);
-
-    uint8_t input = readInputs();
-
-    if (input == 0b00)
-    {
-      state = E_GREEN;
-    }
-    else
-    {
-      state = E_YELLOW;
-    }
-  }
-
-
-
-  // EAST YELLOW
-  else if (state == E_YELLOW)
-  {
-    setLights(E_YELLOW);
-
-    delay(1000);
-
-    state = ALL_RED_2;
-  }
-
-
-  // ALL RED 2
-  else if (state == ALL_RED_2)
-  {
-    setLights(ALL_RED_2);
-
-    delay(500);
-
-    uint8_t input = readInputs();
-
-    if (input == 0b10)
-    {
-      // North car only
-      state = N_GREEN;
-    }
-    else if (input == 0b11)
-    {
-      // Both have cars
-      state = N_GREEN;
-    }
-    else
-    {
-      // East only or nobody
-      state = E_GREEN;
-    }
-  }
+  setLights(FSM[currentState].ledOutput);
 }
